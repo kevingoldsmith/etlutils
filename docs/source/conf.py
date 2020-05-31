c1d87dcced7c62ed1f5623377328eeaba1f90e8b
Kevin Goldsmith <kevin@unitcircle.com>
2020-05-31 13:16:39 -0700

    fix the missing contents.rst file issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,5 +57,6 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+master_doc = 'index'
 
 # -- Extension configuration -------------------------------------------------
